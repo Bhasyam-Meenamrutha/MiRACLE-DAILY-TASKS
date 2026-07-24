25d4042fa770f4c1bae0af18fd5b4c1319453cea
Bhasyam-Meenamrutha <bhasyam82@gmail.com>
2026-07-24 11:32:09 +0530

    Task 1 completed

--- exception_handling_one/ThrowsEx.java
+++ exception_handling_one/ThrowsEx.java
@@ -1,5 +1,18 @@
 package exception_handling_one;
 
 public class ThrowsEx {
+	 static void divide(int a, int b) throws ArithmeticException {
+        System.out.println("Result = " + (a / b));
+    }
 
+    public static void main(String[] args) {
+
+        try {
+            divide(10, 0);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero.");
+        }
+
+        System.out.println("Program continues...");
+    }
 }
